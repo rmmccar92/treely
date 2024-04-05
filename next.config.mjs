c09d521cf8d2579edccf2714c6717700c1759d59
Ryan McCarthy <rmmccar92@gmail.com>
2024-04-04 22:44:46 -0400

    added font

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
