5582095508168501ca795e39306d6f9dff5d0f13
Ryan McCarthy <rmmccar92@gmail.com>
2024-04-04 21:37:15 -0400

    layout grid started

--- app/_components/Section.tsx
+++ app/_components/Section.tsx
@@ -1,9 +1,14 @@
 import type { FC } from "react";
 import styles from "./styles/section.module.css";
+import { Grid } from "./Layout-Grid";
 
 interface SectionProps {}
 
 const Section: FC<SectionProps> = ({}) => {
-  return <div className={styles.sectionMain}></div>;
+  return (
+    <div className={styles.sectionMain}>
+      <Grid />
+    </div>
+  );
 };
 export default Section;
