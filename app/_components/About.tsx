1e71f6d4b2968966dc1a262e97424a7a6772dba2
Ryan McCarthy <rmmccar92@gmail.com>
2024-04-16 15:15:19 -0400

    cards created

--- app/_components/About.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from "react";
-import styles from "./styles/section.module.css";
-
-interface AboutProps {}
-
-export const About: FC<AboutProps> = ({}) => {
-  return <div className="mt-10">About Us</div>;
-};
