264596ad082c5b89ab2a3a2808986f58d1fd8bca
Sean M <mctastic2121@gmail.com>
2024-04-16 18:54:03 -0400

    no idea what I changed

--- app/page.tsx
+++ app/page.tsx
@@ -33,12 +33,14 @@ export default function Home() {
             // image="https://plus.unsplash.com/premium_photo-1661398317164-4978b1a5d4ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
           <Image 
+          className="rounded-xl"
           src="https://plus.unsplash.com/premium_photo-1661398317164-4978b1a5d4ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="holiday stock image"
           width={950}
           height={1}/>
           </div>
           <div className="flex items-center flex-wrap h-100 mt-10 mx-auto">
             <Image 
+            className="rounded-xl"
             src="https://images.unsplash.com/photo-1576995083066-f7bf549dc771?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt= "holiday stock image"
             width={950}
@@ -56,6 +58,7 @@ export default function Home() {
             // image="https://images.unsplash.com/photo-1546245906-811a31d5f4d0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
           <Image
+          className="rounded-xl"
           src = "https://images.unsplash.com/photo-1546245906-811a31d5f4d0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="holiday stock image"
           width={950}
