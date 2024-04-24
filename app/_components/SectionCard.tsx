1ed30d0b95b50811c693bad1957791ae14dabb6e
Sean M <mctastic2121@gmail.com>
2024-04-24 16:28:36 -0400

    added media queries for section cards

--- app/_components/SectionCard.tsx
+++ app/_components/SectionCard.tsx
@@ -26,7 +26,7 @@ export const SectionCard: React.FC<SectionCardProps> = ({
         <div
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}
-          className=" rounded-r-3xl border-2 w-full flex flex-col overflow-hidden items-center justify-center backdrop-blur-xl bg-white/30 gap-4 mx-auto p-8 relative"
+          className="processSec rounded-r-3xl border-2 w-full flex flex-col overflow-hidden items-center justify-center backdrop-blur-xl bg-white/30 gap-4 mx-auto p-8 relative"
         >
           <div className=" w-[40rem] flex flex-col">
             <h1 className="w-full md:text-7xl text-3xl lg:text-6xl font-bold text-center text-black relative z-50">
@@ -73,7 +73,7 @@ export const SectionCard: React.FC<SectionCardProps> = ({
         <div
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}
-          className=" rounded-l-3xl border-2 h-[40rem] w-[50rem] flex flex-col lg:flex-column overflow-hidden items-center justify-center backdrop-blur-xl bg-white/30 gap-4 mx-auto px-8 relative"
+          className="regSec rounded-l-3xl border-2 h-[40rem] w-[50rem] flex flex-col lg:flex-column overflow-hidden items-center justify-center backdrop-blur-xl bg-white/30 gap-4 mx-auto px-8 relative"
         >
           <h1 className="w-full md:text-7xl text-3xl lg:text-6xl font-bold text-center text-black relative z-50">
             {title || "Title"}
