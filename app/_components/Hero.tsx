c7adcc63f30db7fa6b94e8d6061d83f8540b8188
Sean M <mctastic2121@gmail.com>
2024-04-26 13:19:53 -0400

    added background color behind hero video

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,20 +1,36 @@
 "use client";
 
 import Image from "next/image";
-
+import { useState, useEffect } from "react";
 import styles from "./styles/hero.module.css";
 
 export function Hero() {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    }
+  }, []);
+
   return (
     <div className={styles.hero}>
+      {isMobile ? 
+      <video
+      className={styles.heroVid}
+      autoPlay
+      muted
+      loop
+      src="/adobe_tree_video.mov"
+      poster="/snow_2.png"
+      /> :
       <video
         className={styles.heroVid}
         autoPlay
         muted
         loop
         src="/adobe_tree_video.mov"
-        poster="/snow_2.png"
       />
+      }
       <Image
         src="/treely_logo.png"
         alt="treely logo"
