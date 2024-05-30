b4fa388fec126d9af9de0621f4f8e147ea060728
Ryan McCarthy <rmmccar92@gmail.com>
2024-05-30 18:23:00 -0400

    added styling to themenav

--- app/_components/ThemeNav.tsx
+++ app/_components/ThemeNav.tsx
@@ -12,15 +12,15 @@ export const ThemeNav: FC<ThemeNavProps> = ({}) => {
     <div
       className={cn(
         styles.main,
-        "rounded-3xl border-2 h-[40rem] w-full flex items-center justify-center backdrop-blur-xl bg-white/30 gap-8"
+        "rounded-3xl border-2 h-[40rem] w-full flex items-start justify-center backdrop-blur-xl bg-white/30 gap-8 p-4 pt-[6rem]  "
       )}
     >
-      <div className="flex-col items-center justify-center text-center">
+      <div className="flex-col items-center justify-center text-center hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer">
         <button
           onClick={() => router.push("/over-the-river-and-through-the-woods")}
         >
           <Image
-            className="rounded-3xl border-2 mb-4"
+            className="rounded-3xl border-2 mb-4 hover:brightness-110 transition-all duration-300 ease-in-out"
             src="/Rancho_Bernardo/images/IMG_7197.JPG"
             alt="Over the River and Through the Woods"
             width={350}
@@ -31,10 +31,10 @@ export const ThemeNav: FC<ThemeNavProps> = ({}) => {
           </span>
         </button>
       </div>
-      <div className="flex-col items-center justify-center text-center">
+      <div className="flex-col items-center justify-center text-center hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer">
         <button onClick={() => router.push("/industries-finest")}>
           <Image
-            className="rounded-3xl border-2 mb-4"
+            className="rounded-3xl border-2 mb-4 hover:brightness-110 transition-all duration-300 ease-in-out"
             src="/450/images/IMG_7282.JPG"
             alt="Over the River and Through the Woods"
             width={350}
@@ -43,10 +43,10 @@ export const ThemeNav: FC<ThemeNavProps> = ({}) => {
           <span className="mt-2 text-xl font-bold">Industries Finest</span>
         </button>
       </div>
-      <div className="flex-col items-center justify-center text-center">
+      <div className="flex-col items-center justify-center text-center hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer">
         <button onClick={() => router.push("/hooville")}>
           <Image
-            className="rounded-3xl border-2 mb-4"
+            className="rounded-3xl border-2 mb-4 hover:brightness-110 transition-all duration-300 ease-in-out"
             src="/550/images/IMG_7261.JPG"
             alt="Over the River and Through the Woods"
             width={350}
@@ -55,10 +55,10 @@ export const ThemeNav: FC<ThemeNavProps> = ({}) => {
           <span className="mt-2 text-xl font-bold">Hooville</span>
         </button>
       </div>
-      <div className="flex-col items-center justify-center text-center">
+      <div className="flex-col items-center justify-center text-center hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer">
         <button onClick={() => router.push("/golden-age")}>
           <Image
-            className="rounded-3xl border-2 mb-4"
+            className="rounded-3xl border-2 mb-4 hover:brightness-110 transition-all duration-300 ease-in-out"
             src="/C&W/images/IMG_0250.JPG"
             alt="Over the River and Through the Woods"
             width={350}
@@ -67,10 +67,10 @@ export const ThemeNav: FC<ThemeNavProps> = ({}) => {
           <span className="mt-2 text-xl font-bold">Golden Age</span>
         </button>
       </div>
-      <div className="flex-col items-center justify-center text-center">
+      <div className="flex-col items-center justify-center text-center hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer">
         <button onClick={() => router.push("/snowdrift")}>
           <Image
-            className="rounded-3xl border-2 mb-4"
+            className="rounded-3xl border-2 mb-4 hover:brightness-110 transition-all duration-300 ease-in-out"
             src="/Lebeau/images/IMG_0245.JPG"
             alt="Over the River and Through the Woods"
             width={350}
